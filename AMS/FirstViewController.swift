fd96db290a5372442e886925b40de0af3cb2d2f6
abdul.ashif <abdul.ashif@dot.abudhabi.ae>
2019-12-30 15:01:00 +0400

    Initial Commit

---
+++ AMS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  AMS
+//
+//  Created by DOT Developer DOT on 30/12/2019.
+//  Copyright © 2019 Doodle. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
